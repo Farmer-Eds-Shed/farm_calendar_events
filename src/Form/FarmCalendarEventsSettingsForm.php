415b094ea022f6f865a12d90e3a5b864cfc206be
Farmer Ed <63633329+Farmer-Eds-Shed@users.noreply.github.com>
2022-05-22 20:51:18 +0100

    Add files via upload

---
+++ src/Form/FarmCalendarEventsSettingsForm.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Drupal\farm_Calendar_Events\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a farm calender settings form.
+ */
+class FarmCalendarEventsSettingsForm extends ConfigFormbase {
+
+  /**
+   * Config settings.
+   *
+   * @var string
+   */
+  const SETTINGS = 'farm_calendar_events.settings';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'farm_calendar_events_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      static::SETTINGS,
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateinterface $form_state) {
+    $config = $this->config(static::SETTINGS);
+
+	
+	$form['calendar_id'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Calendar ID'),
+      '#description' => $this->t('Enter Calendar ID.'),
+	  '#default_value' => $config->get('calendar_id'),
+    ];
+	
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->configFactory->getEditable(static::SETTINGS)
+	  ->set('calendar_id', $form_state->getValue('calendar_id'))
+      ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
